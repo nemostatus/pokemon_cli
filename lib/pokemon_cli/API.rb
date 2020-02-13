446337e1e7a0435c6a1391a6f62206ac2ca058c4
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-13 05:59:24 +0000

    api method complete

--- lib/pokemon_cli/API.rb
+++ lib/pokemon_cli/API.rb
@@ -2,6 +2,9 @@ class API
  def self.get_data 
    response = RestClient.get("https://pokeapi.co/api/v2/pokemon/")
    pokemon_array = JSON.parse(response.body)["results"]
- end 
+     pokemon_array.each do |pokemon|
+      Pokemon.new(pokemon)
+    end 
+  end 
  end 
    
