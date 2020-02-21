e32177aae3250ad1c20fddacbc27685ca1b46bb3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-21 04:49:59 +0000

    refactoring

--- lib/pokemon_cli/API.rb
+++ lib/pokemon_cli/API.rb
@@ -1,27 +1,10 @@
 class API 
- def self.get_data 
-   response = RestClient.get("https://pokeapi.co/api/v2/pokemon/")
-   pokemon_array = JSON.parse(response.body)["results"]
+ def self.get_data(url=,key)
+   response = RestClient.get("#{url}")
+   pokemon_array = JSON.parse(response.body)["#{key}"]
      pokemon_array.each do |pokemon|
       Pokemon.new(pokemon)
     end 
   end 
-  def self.more_data(name,stat)
-    response = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{name}/")
-    pokemon_stats = JSON.parse(response.body)["#{stat}"]
-    if stat == "weight" 
-     
-     puts  pokemon_stats = JSON.parse(response.body)["#{stat}"]
-      elsif stat == "height"
-       puts pokemon_stats = JSON.parse(response.body)["#{stat}"]
-       elsif stat == "base_experience"
-         PokemonCli::CLI.pokemon_battle << JSON.parse(response.body)["#{stat}"]
-       puts pokemon_stats = JSON.parse(response.body)["#{stat}"]
-    else
-    pokemon_stats.each do |pokemon|
-      Pokemon.stats << Pokemon.new(pokemon)
-       end
-  end  
- end
-end
+
    
