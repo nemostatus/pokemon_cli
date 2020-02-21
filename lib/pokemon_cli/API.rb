4a88d86b396d1553e9a99ce7185a4609afbaacc3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-21 10:29:42 +0000

    api update

--- lib/pokemon_cli/API.rb
+++ lib/pokemon_cli/API.rb
@@ -2,11 +2,9 @@ class API
  def self.get_data(name=nil,key)
 response = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{name}")
    pokemon_array = JSON.parse(response.body)["#{key}"]
-   if name == nil 
-     pokemon_array.each do |poke|
+ pokemon_array.each do |poke|
       Pokemon.new(poke)
-    else
-   end   
+ end   
 end
  end  
  
