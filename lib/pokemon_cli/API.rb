41df0013fc7db4b72ace178ecb329578ae860c51
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-21 04:58:18 +0000

    removed second api call, putting all responsibility on one api call

--- lib/pokemon_cli/API.rb
+++ lib/pokemon_cli/API.rb
@@ -1,6 +1,6 @@
 class API 
- def self.get_data(url=,key)
-   response = RestClient.get("#{url}")
+ def self.get_data(name=nil,key="results")
+response = RestClient.get("https://pokeapi.co/api/v2/pokemon/#{name}")
    pokemon_array = JSON.parse(response.body)["#{key}"]
      pokemon_array.each do |pokemon|
       Pokemon.new(pokemon)
